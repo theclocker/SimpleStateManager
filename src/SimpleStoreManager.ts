1f493988d2f0213dbed048573a992b0e72ca736e
theclocker <yonatan.vega.bla@gmail.com>
2018-02-27 20:03:10 +0200

    Removed singleton in store() function

--- src/SimpleStoreManager.ts
+++ src/SimpleStoreManager.ts
@@ -1,12 +1,13 @@
 import { Factory } from './store/Factory';
 
-export namespace StoreBox {
+/*export namespace StoreBox {
     export const storeFactories: {[store: string]: Factory} = {};
-}
+}*/
 
 export function store(name: string): Factory {
-    if (!StoreBox.storeFactories.hasOwnProperty(name)) {
+    /*if (!StoreBox.storeFactories.hasOwnProperty(name)) {
         StoreBox.storeFactories[name] = new Factory(name);
     }
-    return StoreBox.storeFactories[name];
+    return StoreBox.storeFactories[name];*/
+    return new Factory(name);
 }
